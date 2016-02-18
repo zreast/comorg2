580395fa32d8790c65d755a528fdadbc6b7f941e
halloweenrr <save14597@gmail.com>
2016-02-18 15:47:55 +0700

    Screen Menu nearly complete!?
    You can press esc to exit program and press 1,2 and 3 to start game.

--- Techapple.net/Tasm/urman.asm
+++ Techapple.net/Tasm/urman.asm
@@ -4,7 +4,8 @@ row      		db      23
 column    		db      40
 rowsub			db		24
 chartemp		db		85d
-
+key db 0
+ msg2 	db "  1:easy / 2:normal / 3:hard "
 .code
     org     0100h
 main:   
@@ -12,8 +13,107 @@ main:
 	mov     ah, 00h         ; Set to 80x25
     mov     al, 03h
     int     10h
+	
+			
+	call	screenmenu
+		
+       ;======================SCREEN MENU=================
+	
+screenmenu:   
 
+		mov     ah, 00h         ; Set to 80x25
+		mov     al, 03h
+		int     10h
+		
+		mov     ah, 01h
+		mov     cx, 2607h  	; hide cursr
+         int     10h
+		
+		mov		si,0100b
+		mov 	al,1
+		mov 	bh,0
+		mov 	bl, 4
+		mov 	cx, 44		;legnth of char
+		mov 	dl,	19		;column
+		mov 	dh, 10 		;row
+		push 	cs
+		push 	es
+		mov 	bp,  offset msg1
+		mov 	ah, 13h
+		int 	10h
+		jmp 	msg1end
+		 msg1 	db " Please choose your difficulty (ESC to exit)"
+		
+		msg1end:
+			
+		add 	si,1
+		mov 	al,0
+		mov 	bh,0
+		mov 	bl, [si]
+		mov 	cx, 29		;legnth of char
+		mov 	dl,	26		;column
+		mov 	dh, 12 		;row
+		push 	cs
+		push 	es
+		mov 	bp,  offset msg2
+		mov 	ah, 13h
+		int 	10h
+			
+		mov		ah,6			;get char from keyboard
+		mov		dl,255			;INPUTMODE
+		int		21h	
+		cmp		al,27			;COMPARE WITH ESC
+		jne		no1			;IF NOT EQUAL GO CHANGE COLOR
+		je		outprog			;IF EQUAL END PROGRAM		
+		no1:
+		
+		cmp		al,49			;COMPARE WITH 1
+		jne		no2				;if not choose go to no2 for change color \continune
+		je		rosesfall		;if chose 1 go to xwing
+		no2:
+		
+		cmp		al,50			;COMPARE WITH 2
+		jne		no3				;if not choose go to no2 for change color \continune
+		je		rosesfall		;if chose 1 go to xwing	
+		no3:
+		
+		cmp		al,51			;COMPARE WITH 3
+		jne	delay2			;if not choose go to no2 for change color \continune
+		je		rosesfall		;if chose 1 go to xwing		
+		
+		delay2:	   				;DELAY WHO CARE
+		mov 	di, 10
+		mov 	ah, 0
+		int 	1Ah
+		mov 	bx, dx
+		call	delayy2
+		
+		delayy2:
+        mov 	ah, 0
+        int 	1Ah
+        sub 	dx, bx
+        cmp 	di, dx
+        ja 		delayy2
+		
+		
+	jmp		msg1end			;CONTINUE TO CHANGE COLOR
+				
+		outprog:
+		mov     ah, 00h         ; Set to 80x25
+        mov     al, 03h 
+        int     10h  
+		int		20h
+	ret			;return to call
+
+	;================MENU End here=======================
+	
     rosesfall:
+			mov     ah, 00h         ; Set to 80x25
+			mov     al, 03h
+			int     10h
+			
+			
+			
             mov     ah, 02h     ;Move cursor XY
             mov     bh, 00h
             mov     dh, row      ;y
